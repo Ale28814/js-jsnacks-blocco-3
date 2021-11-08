512f1245724770e8003911df31b152f056c3817c
Ale28814 <alened928@gmail.com>
2021-11-08 18:00:21 +0100

    basketball player array obj

---
+++ snack-1/main.js
@@ -0,0 +1,12 @@
+//Creare un oggetto di dati di un basketball player
+const basketBallPlayer = [
+    {
+        gamerCode: '',
+        name: 'Kent',
+        lastName: 'Bazemore',
+        age: 32,
+        mPp: '',
+        pSt3P: '',
+
+    },
+];
